62c116597efd2a22fa0fe36591f93fe7d11f86f0
Adarsh-MR <adarshmr1998@gmail.com>
2017-11-03 18:48:36 +0000

    CardinalOTA: Add makefile to build inline
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := CardinalOTA
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_MANIFEST_FILE := app/src/main/AndroidManifest.xml
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v7-appcompat \
+    android-support-v7-cardview \
+    android-support-design \
+    android-support-annotations \
+    android-support-v4 \
+    htmlparser
+
+LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main/java)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/cardview/res \
+    frameworks/support/design/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.appcompat:android.support.v7.cardview:android.support.design
+
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := htmlparser:app/libs/htmlparser.jar
+
+include $(BUILD_MULTI_PREBUILT)
